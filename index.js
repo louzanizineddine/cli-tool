b684dab0a630ab39b4734231b6dc6f06a50af59a
“louzanizineddine” <louzanizineddine@gmail.com>
2022-02-02 16:11:33 +0000

    first question the every estin student can answer

---
+++ index.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node 
+
+import chalk from 'chalk'
+import chalkAnimation from 'chalk-animation'
+import figlet from 'figlet';
+import gradient from 'gradient-string';
+import inquirer from 'inquirer'
+import { createSpinner } from 'nanospinner';
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r , ms)) 
+
+async function welcome (){
+    const rainBowTitle = chalkAnimation.rainbow(`
+        who want to be a javascript millionnaire
+    `)
+
+    await sleep();
+
+    rainBowTitle.stop();
+
+    console.log(`
+        ${chalk.bgBlue('HOW TO PLAY')}
+        I am a precess in  your computer
+        If you get any question wrong I will be ${chalk.bgRed('Killed')}
+        so get all the questions ${chalk.bgGreen('right')}
+    `);
+}
+
+async function askName () {
+    const answers = await inquirer.prompt({
+        name: 'player_name',
+        type: 'input',
+        message: 'What is your name',
+        default() {
+            return 'player'
+        },
+    });
+
+    playerName = answers.player_name
+}
+
+async function askQst1() {
+    const answers = await inquirer.prompt({
+        name: 'question_1',
+        type : 'list',
+        message: 'How many times THE ESTIN changed the exam planning \n',
+        choices : [
+            'One time',
+            'Two time',
+            'Three time',
+            'Fouth time',
+            'Only Allah Knows'
+        ],
+    });
+
+    return handleAnswer(answers.question_1 == 'Only Allah Knows')
+}
+
+async function handleAnswer(isCorrect) {
+    const spinner = createSpinner('checking answer...').start()
+    await sleep();
+    if (isCorrect){
+        spinner.success({text : `Nice Work ${playerName}. That's a legit answer`})
+        await sleep();
+        winner()
+    }else {
+        spinner.error({text: "Game over man"})
+        process.exit(1)
+    }
+}
+
+function winner(){
+    console.clear()
+    const msg = `Congrats ${playerName}`;
+
+    figlet(msg , (err , data) => {
+        console.log(gradient.pastel.multiline(data))
+    })
+}
+
+await welcome()
+await askName()
+await askQst1()
